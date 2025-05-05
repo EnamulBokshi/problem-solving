0a08ab125eeda84a0544f12f45ac1441dfd41b41
enamulbokshi <enamulbokshi@gmail.com>
2025-05-05 23:31:40 +0600

    initial solve of count and say problem of leetcode

--- leetcode/count_and_say/solution.py
+++ leetcode/count_and_say/solution.py
@@ -1,11 +1,54 @@
 
-n = int(input())
+# n = int(input())
 
 
-def createMap(number:int) :
-    pass
+def createMap(number:str) :
+    pairList = []
+    
+    i,j = 0,0
 
-createMap("khello")
+    while(i<len(number)):
+        count = 1
+        pair = {number[i]: count}
+        for j in range(i+1, len(number)):
+            if(number[i] != number[j]):
+                break
+            count += 1
+        
+        print("i: ",i)
 
-def createList(arr:list):
-    pass
+        pair[number[i]] = count
+        print("pair: ", pair)
+        i += count 
+        pairList.append(pair)
+    return pairList
+
+
+
+
+def generateNumberString(arr:dict):
+    result = ''
+    for item in arr:
+        for keys,value in item.items():
+            stringStream = keys+str(value)
+            result += stringStream
+    return result
+
+
+def countAndSey(n:int) -> str:
+    finalResult = ''
+    numberSting = '1'
+    while(n):
+        numberList = createMap(numberSting)
+        numberSting = generateNumberString(numberList)
+
+        n -= 1
+    return numberSting
+
+res = countAndSey(4)
+print(res)
+
+
+
+# countSayResult = generateNumberString(mappedList)
+
